240e2d7f52156fb6b63bb93a736b63162839085e
parroit <andrea.parodi@ebansoftware.net>
2013-10-22 15:03:53 +0200

    ported test to mocha. added grunt file

--- test/index.js
+++ test/index.js
@@ -1,178 +1,180 @@
-var test = require("tape")
+var assert = require("assert");
+var expect = require("expect.js");
 
-var format = require("../index.js")
 
-test("Named arguments are replaced", function (assert) {
-    var result = format("Hello {name}, how are you?", { name: "Mark" })
-    assert.equal(result, "Hello Mark, how are you?")
-    assert.end()
-})
+var format = require("../index.js");
 
-test("Named arguments at the start of strings are replaced",
-    function (assert) {
+it("Named arguments are replaced", function () {
+    var result = format("Hello {name}, how are you?", { name: "Mark" });
+    expect(result).to.be.equal("Hello Mark, how are you?");
+   
+});
+
+it("Named arguments at the start of strings are replaced",
+    function () {
         var result = format("{likes} people have liked this", {
             likes: 123
-        })
+        });
 
-        assert.equal(result, "123 people have liked this")
-        assert.end()
-    })
+        expect(result).to.be.equal("123 people have liked this");
+       
+    });
 
-test("Named arguments at the end of string are replaced",
-    function (assert) {
+it("Named arguments at the end of string are replaced",
+    function () {
         var result = format("Please respond by {date}", {
             date: "01/01/2015"
-        })
+        });
 
-        assert.equal(result, "Please respond by 01/01/2015")
-        assert.end()
-    })
+        expect(result).to.be.equal("Please respond by 01/01/2015");
+       
+    });
 
-test("Multiple named arguments are replaced", function (assert) {
+it("Multiple named arguments are replaced", function () {
     var result = format("Hello {name}, you have {emails} new messages", {
         name: "Anna",
         emails: 5
-    })
+    });
 
-    assert.equal(result, "Hello Anna, you have 5 new messages")
-    assert.end()
-})
+    expect(result).to.be.equal("Hello Anna, you have 5 new messages");
+   
+});
 
-test("Missing named arguments become 0 characters", function (assert) {
-    var result = format("Hello{name}, how are you?", {})
-    assert.equal(result, "Hello, how are you?")
-    assert.end()
-})
+it("Missing named arguments become 0 characters", function () {
+    var result = format("Hello{name}, how are you?", {});
+    expect(result).to.be.equal("Hello, how are you?");
+   
+});
 
-test("Named arguments can be escaped", function (assert) {
-    var result = format("Hello {{name}}, how are you?", { name: "Mark" })
-    assert.equal(result, "Hello {name}, how are you?")
-    assert.end()
-})
+it("Named arguments can be escaped", function () {
+    var result = format("Hello {{name}}, how are you?", { name: "Mark" });
+    expect(result).to.be.equal("Hello {name}, how are you?");
+   
+});
 
-test("Array arguments are replaced", function (assert) {
-    var result = format("Hello {0}, how are you?", ["Mark"])
-    assert.equal(result, "Hello Mark, how are you?")
-    assert.end()
-})
+it("Array arguments are replaced", function () {
+    var result = format("Hello {0}, how are you?", ["Mark"]);
+    expect(result).to.be.equal("Hello Mark, how are you?");
+   
+});
 
-test("Array arguments at the start of strings are replaced",
-    function (assert) {
-        var result = format("{0} people have liked this", [123])
+it("Array arguments at the start of strings are replaced",
+    function () {
+        var result = format("{0} people have liked this", [123]);
 
-        assert.equal(result, "123 people have liked this")
-        assert.end()
-    })
+        expect(result).to.be.equal("123 people have liked this");
+       
+    });
 
-test("Array arguments at the end of string are replaced",
-    function (assert) {
-        var result = format("Please respond by {0}", ["01/01/2015"])
+it("Array arguments at the end of string are replaced",
+    function () {
+        var result = format("Please respond by {0}", ["01/01/2015"]);
 
-        assert.equal(result, "Please respond by 01/01/2015")
-        assert.end()
-    })
+        expect(result).to.be.equal("Please respond by 01/01/2015");
+       
+    });
 
-test("Multiple array arguments are replaced", function (assert) {
+it("Multiple array arguments are replaced", function () {
     var result = format("Hello {0}, you have {1} new messages", [
         "Anna",
         5
-    ])
+    ]);
 
-    assert.equal(result, "Hello Anna, you have 5 new messages")
-    assert.end()
-})
+    expect(result).to.be.equal("Hello Anna, you have 5 new messages");
+   
+});
 
-test("Missing array arguments become 0 characters", function (assert) {
-    var result = format("Hello{0}, how are you?", [])
-    assert.equal(result, "Hello, how are you?")
-    assert.end()
-})
+it("Missing array arguments become 0 characters", function () {
+    var result = format("Hello{0}, how are you?", []);
+    expect(result).to.be.equal("Hello, how are you?");
+   
+});
 
-test("Array arguments can be escaped", function (assert) {
-    var result = format("Hello {{0}}, how are you?", ["Mark"])
-    assert.equal(result, "Hello {0}, how are you?")
-    assert.end()
-})
+it("Array arguments can be escaped", function () {
+    var result = format("Hello {{0}}, how are you?", ["Mark"]);
+    expect(result).to.be.equal("Hello {0}, how are you?");
+   
+});
 
-test("Array keys are not accessible", function (assert) {
-    var result = format("Function{splice}", [])
-    assert.equal(result, "Function")
-    assert.end()
-})
+it("Array keys are not accessible", function () {
+    var result = format("Function{splice}", []);
+    expect(result).to.be.equal("Function");
+   
+});
 
-test("Listed arguments are replaced", function (assert) {
-    var result = format("Hello {0}, how are you?", "Mark")
-    assert.equal(result, "Hello Mark, how are you?")
-    assert.end()
-})
+it("Listed arguments are replaced", function () {
+    var result = format("Hello {0}, how are you?", "Mark");
+    expect(result).to.be.equal("Hello Mark, how are you?");
+   
+});
 
-test("Listed arguments at the start of strings are replaced",
-    function (assert) {
-        var result = format("{0} people have liked this", 123)
+it("Listed arguments at the start of strings are replaced",
+    function () {
+        var result = format("{0} people have liked this", 123);
 
-        assert.equal(result, "123 people have liked this")
-        assert.end()
-    })
+        expect(result).to.be.equal("123 people have liked this");
+       
+    });
 
-test("Listed arguments at the end of string are replaced",
-    function (assert) {
-        var result = format("Please respond by {0}", "01/01/2015")
+it("Listed arguments at the end of string are replaced",
+    function () {
+        var result = format("Please respond by {0}", "01/01/2015");
 
-        assert.equal(result, "Please respond by 01/01/2015")
-        assert.end()
-    })
+        expect(result).to.be.equal("Please respond by 01/01/2015");
+       
+    });
 
-test("Multiple listed arguments are replaced", function (assert) {
+it("Multiple listed arguments are replaced", function () {
     var result = format("Hello {0}, you have {1} new messages",
         "Anna",
-        5)
+        5);
 
-    assert.equal(result, "Hello Anna, you have 5 new messages")
-    assert.end()
-})
+    expect(result).to.be.equal("Hello Anna, you have 5 new messages");
+   
+});
 
-test("Missing listed arguments become 0 characters", function (assert) {
-    var result = format("Hello{1}, how are you?", "no")
-    assert.equal(result, "Hello, how are you?")
-    assert.end()
-})
+it("Missing listed arguments become 0 characters", function () {
+    var result = format("Hello{1}, how are you?", "no");
+    expect(result).to.be.equal("Hello, how are you?");
+   
+});
 
-test("Listed arguments can be escaped", function (assert) {
-    var result = format("Hello {{0}}, how are you?", "Mark")
-    assert.equal(result, "Hello {0}, how are you?")
-    assert.end()
-})
+it("Listed arguments can be escaped", function () {
+    var result = format("Hello {{0}}, how are you?", "Mark");
+    expect(result).to.be.equal("Hello {0}, how are you?");
+   
+});
 
-test("Allow null data", function (assert) {
-    var result = format("Hello{0}", null)
-    assert.equal(result, "Hello")
-    assert.end()
-})
+it("Allow null data", function () {
+    var result = format("Hello{0}", null);
+    expect(result).to.be.equal("Hello");
+   
+});
 
-test("Allow undefined data", function (assert) {
-    var result1 = format("Hello{0}")
-    var result2 = format("Hello{0}", undefined)
-    assert.equal(result1, "Hello")
-    assert.equal(result2, "Hello")
-    assert.end()
-})
+it("Allow undefined data", function () {
+    var result1 = format("Hello{0}");
+    var result2 = format("Hello{0}", undefined);
+    expect(result1).to.be.equal( "Hello");
+    expect(result2).to.be.equal( "Hello");
+   
+});
 
-test("Null keys become 0 characters", function (assert) {
-    var result1 = format("Hello{name}", { name: null })
-    var result2 = format("Hello{0}", [null])
-    var result3 = format("Hello{0}{1}{2}", null, null, null)
-    assert.equal(result1, "Hello")
-    assert.equal(result2, "Hello")
-    assert.equal(result3, "Hello")
-    assert.end()
-})
+it("Null keys become 0 characters", function () {
+    var result1 = format("Hello{name}", { name: null });
+    var result2 = format("Hello{0}", [null]);
+    var result3 = format("Hello{0}{1}{2}", null, null, null);
+    expect(result1).to.be.equal( "Hello");
+    expect(result2).to.be.equal( "Hello");
+    expect(result3).to.be.equal( "Hello");
+   
+});
 
-test("Undefined keys become 0 characters", function (assert) {
-    var result1 = format("Hello{firstName}{lastName}", { name: undefined })
-    var result2 = format("Hello{0}{1}", [undefined])
-    var result3 = format("Hello{0}{1}{2}", undefined, undefined)
-    assert.equal(result1, "Hello")
-    assert.equal(result2, "Hello")
-    assert.equal(result3, "Hello")
-    assert.end()
-})
+it("Undefined keys become 0 characters", function () {
+    var result1 = format("Hello{firstName}{lastName}", { name: undefined });
+    var result2 = format("Hello{0}{1}", [undefined]);
+    var result3 = format("Hello{0}{1}{2}", undefined, undefined);
+    expect(result1).to.be.equal( "Hello");
+    expect(result2).to.be.equal( "Hello");
+    expect(result3).to.be.equal( "Hello");
+   
+});
